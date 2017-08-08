fd3dd4a63297b942a90c375067002e8313223e04
Dhruvs97 <dhruvsax007@gmail.com>
2017-08-08 19:14:01 +0530

    Add files via upload

---
+++ ProgrammingAssignment2.R
@@ -0,0 +1,30 @@
+## Functions to cache an inversed matrix
+## The function creates a matrix object that can cache the inverse
+
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(solveMatrix) inv <<- solveMatrix
+  getInverse <- function() inv
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+}
+
+
+## Computes inverse of matrix
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setInverse(inv)
+  inv  
+}
